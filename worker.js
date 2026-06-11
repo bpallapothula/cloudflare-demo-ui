d7b83ae53da8b970adfa56579b94c1e37a44575b
Balakrishna <bpallapothula@wearebarbarian.com>
2026-06-11 19:18:44 +0530

    back to worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,19 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
     const cookie = request.headers.get("Cookie") || "";
 
-    return new Response(`Cookie header: ${cookie}`);
+    // Allow login page
+    if (url.pathname === "/login.html") {
+      return env.ASSETS.fetch(request);
+    }
+
+    // Redirect if not authenticated
+    if (!cookie.includes("auth=true")) {
+      return Response.redirect(`${url.origin}/login.html`, 302);
+    }
+
+    // Serve requested HTML/CSS/JS asset
+    return env.ASSETS.fetch(request);
   }
 };
