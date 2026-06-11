b8be7184c9cc9eb1b98f93a1084d7f75538c44a8
Balakrishna <bpallapothula@wearebarbarian.com>
2026-06-11 15:16:24 +0530

    added middleware for testing purpose

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,15 +1,3 @@
-export async function onRequest({ request, next }) {
-  const cookie = request.headers.get("Cookie") || "";
-  const url = new URL(request.url);
-
-  // Allow login page
-  if (url.pathname === "/login.html") {
-    return next();
-  }
-
-  if (!cookie.includes("auth=true")) {
-    return Response.redirect(`${url.origin}/login.html`, 302);
-  }
-
-  return next();
+export async function onRequest() {
+  return new Response("Middleware is running!");
 }
