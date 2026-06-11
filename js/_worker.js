e55ea81b94e10f3d2a59ae16cc4f1ffe893c2bea
Balakrishna <bpallapothula@wearebarbarian.com>
2026-06-11 16:13:19 +0530

    Testing Worker.js

--- js/_worker.js
+++ js/_worker.js
@@ -1,18 +1,5 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
-
-    // Allow login page
-    if (url.pathname === "/login.html") {
-      return fetch(request);
-    }
-
-    const cookie = request.headers.get("Cookie") || "";
-
-    if (!cookie.includes("auth=true")) {
-      return Response.redirect(`${url.origin}/login.html`, 302);
-    }
-
-    return fetch(request);
+    return new Response("Worker is running!");
   }
 };
