be786f2f9f8e4fda04e23b73f9582ab0fe5eb720
Balakrishna <bpallapothula@wearebarbarian.com>
2026-06-11 15:56:01 +0530

    Middleware js updated

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,3 +1,27 @@
-export async function onRequest() {
-  return new Response("Middleware is running!");
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const pathname = url.pathname;
+
+
+  if (pathname === "/" || pathname === "/index.html" || pathname === "/404.html") {
+    return context.next();
+  }
+
+  // Check for the cookie
+  const cookieHeader = context.request.headers.get("cookie") || "";
+  const hasAccess = cookieHeader.includes("wrangler pages deploy public --project-name my-poc");
+
+  if (!hasAccess) {
+
+    const notFoundPage = await context.env.ASSETS.fetch(
+      new URL("/404.html", context.request.url)
+    );
+
+    return new Response(notFoundPage.body, {
+      status: 404,
+      headers: { "Content-Type": "text/html" },
+    });
+  }
+
+  return context.next();
 }
