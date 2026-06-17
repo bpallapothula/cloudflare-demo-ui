7d45e6f1720596a0082663a97e393e0c003ea3fa
Balakrishna <bpallapothula@wearebarbarian.com>
2026-06-17 23:00:56 +0530

    testing worker.js

--- worker.js
+++ worker.js
@@ -3,17 +3,8 @@ export default {
     const url = new URL(request.url);
     const cookie = request.headers.get("Cookie") || "";
 
-    // Allow login page
-    if (url.pathname === "/login.html") {
-      return env.ASSETS.fetch(request);
-    }
-
-    // Redirect if not authenticated
-    if (!cookie.includes("auth=true")) {
-      return Response.redirect(`${url.origin}/login.html`, 302);
-    }
-
-    // Serve requested HTML/CSS/JS asset
-    return env.ASSETS.fetch(request);
+    return new Response(
+      `Path: ${url.pathname}\nCookie: ${cookie || "No Cookie"}`
+    );
   }
 };
